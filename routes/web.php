0a0c1c32f2bbc3125941ee5ad6ec4dc87e270f9e
adachi001 <kd1336242@st.kobedenshi.ac.jp@gmail.com>
2023-12-12 11:57:48 +0900

    auth

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -25,4 +28,10 @@ Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::get('/posts/{post}/comments', [CommentController::class, 'show'])->name('comments.show');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
